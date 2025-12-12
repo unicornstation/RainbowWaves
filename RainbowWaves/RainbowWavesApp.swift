ca04dbc5a2d8cb6045f52a32edca9759c513d8c5
Alyona Voronina <alyona.creativebox@gmail.com>
2025-12-12 17:59:24 +0300

    Dynamic View

--- RainbowWaves/RainbowWavesApp.swift
+++ RainbowWaves/RainbowWavesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RainbowWavesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RainbowWavesDynamicView()
         }
     }
 }
